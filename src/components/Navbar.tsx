38c3e4d04d66242ec87cae25818a224122b5aaeb
TrixodeStudios <trixodestudios@gmail.com>
2025-05-04 22:49:39 -0700

    chore: Comment out theme toggle for hydration debug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,23 +53,23 @@ export default function Navbar() {
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
             {/* Theme Toggle Button */}
-            {mounted && ( // Only render toggle when mounted
-              <button 
-                onClick={toggleTheme}
-                className="mr-4 p-2 rounded-md text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-gray-800"
-                aria-label="Toggle Dark Mode"
-              >
-                {theme === 'dark' ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.706-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 14.95l.707-.707a1 1 0 011.414 1.414l-.707.707a1 1 0 01-1.414-1.414zM5 11a1 1 0 100-2H4a1 1 0 100 2h1zM8 16a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1zM3.536 5.05l.707.707a1 1 0 101.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zM10 18a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
-                  </svg>
-                ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                  </svg>
-                )}
-              </button>
-            )}
+            {/* {mounted && ( // Only render toggle when mounted */}
+            {/*  <button */}
+            {/*    onClick={toggleTheme}*/}
+            {/*    className="mr-4 p-2 rounded-md text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-gray-800"*/}
+            {/*    aria-label="Toggle Dark Mode"*/}
+            {/*  >*/}
+            {/*    {theme === 'dark' ? (*/}
+            {/*      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">*/}
+            {/*        <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.706-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 14.95l.707-.707a1 1 0 011.414 1.414l-.707.707a1 1 0 01-1.414-1.414zM5 11a1 1 0 100-2H4a1 1 0 100 2h1zM8 16a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1zM3.536 5.05l.707.707a1 1 0 101.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zM10 18a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />*/}
+            {/*      </svg>*/}
+            {/*    ) : (*/}
+            {/*      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">*/}
+            {/*        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />*/}
+            {/*      </svg>*/}
+            {/*    )}*/}
+            {/*  </button>*/}
+            {/* )} */}
             
             {/* Sign in Button */}
             <Link
